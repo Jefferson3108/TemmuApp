f7472c6fd413f1fa51e653cf842789e8081d311b
Jefferson <jefferssonico120@gmail.com>
2025-05-02 12:29:15 -0500

    Cambios del codigo

--- Controller/ControllerCustomer.java
+++ Controller/ControllerCustomer.java
@@ -5,5 +5,6 @@ public class ControllerCustomer {
 	public ControllerCustomer() {
 		// TODO Auto-generated constructor stub
 	}
+	public 
 
 }
--- Model/User.java
+++ Model/User.java
@@ -5,35 +5,36 @@ public class User {
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
-public String assingname(String name) {
+	public String assingname(String name) {
+		return name ;
 		
-	}
-	public Integer assignage(Integer age) {
-		
-	}
-	public Integer assignnumber(Integer number) {
-		
-	}
-	public String assigncity(String city) {
-		
-	}
-	public String assignemail(String email) {
-		
-	}
-	private String assignpassword(String password) {
-		
-	}
-	public Boolean createacount(Boolean f) {
-		
-	}
-	public Boolean validateemail(Boolean f) {
-		
-	}
-	private Boolean validatePassword(Boolean f) {
-		
-	}
-	public Boolean validatelogin(Boolean f) {
-		
-	}
+		}
+		public Integer assignage(Integer age) {
+		return age;	
+		}
+		public Integer assignnumber(Integer number) {
+		return number;	
+		}
+		public String assigncity(String city) {
+		return city;	
+		}
+		public String assignemail(String email) {
+		return email;	
+		}
+		private String assignpassword(String password) {
+		return password;	
+		}
+		public Boolean createacount(Boolean f) {
+		return true;	
+		}
+		public Boolean validateemail(Boolean f) {
+		return true;	
+		}
+		private Boolean validatePassword(Boolean f) {
+		return true;	
+		}
+		public Boolean validatelogin(Boolean f) {
+		return true;	
+		}
 
 }
