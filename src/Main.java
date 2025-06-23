08df220477b25df61f45e25cd44201205f777f65
Jefferson <jefferssonico120@gmail.com>
2025-06-23 14:41:41 -0500

    Changes and add interface of select

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 
 import Controller.Register;
 import Controller.Profile;
-
+import View.Registervw;
+import View.Selectvw;
 public class Main {
 
 	public static void main(String[] args) {
 		/*
 		 * Console interface to request data
 		 */
-		System.out.println("======Welcome to register=====\n\n");
+		Selectvw t= new Selectvw();
+		t.setVisible(true);
+		Registervw u= new Registervw();
+		u.setVisible(true);
 		Register rgst = new Register();
 		if(rgst.SelectRol()==1) {
 			Profile prf = new Profile(rgst.getname(), rgst.getage(), rgst.getnumberofphone(), rgst.getcountry(),
